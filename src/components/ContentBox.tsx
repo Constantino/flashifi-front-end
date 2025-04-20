ca2e509ca11eac0e0d11c83ae43810065c9031b7
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2025-04-19 20:07:11 -0600

    Added selects for selecting networks

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,5 +1,12 @@
+import React from 'react';
 import { useState, useEffect } from 'react'
 import '../App.css'
+import Stack from '@mui/joy/Stack';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
+import Box from '@mui/joy/Box';
 
 interface Token {
     symbol: string;
@@ -25,6 +32,37 @@ export const ContentBox = () => {
         setFromToken(toToken)
         setToToken(temp)
     }
+
+    const [chainFrom, setChainFrom] = useState('0')
+    const [chainTo, setChainTo] = useState('1')
+    const [isInProgress, setIsInProgress] = useState(false)
+    const activeAccount = "";
+
+    const handleChangeChainA = (event: ChangeEvent<HTMLSelectElement>) => {
+        // console.log('chain in use: ', chainInUse)
+        console.log("chain dropdown1: ", event.target.value)
+        let chainToUse = Number(event.target.value);
+        setChainTo(chainToUse)
+        setChainFrom(chainToUse == 0 ? 1 : 0)
+        if (chainFrom == 0 && chainToUse == 1) {
+            console.log('request change of network')
+            // console.log("switching to: ", superchainB)
+            //switchChain(superchainB)
+        }
+        if (chainFrom == 1 && chainToUse == 0) {
+            console.log('request change of network')
+            // console.log("switching to: ", superchainA)
+            //switchChain(superchainA)
+        }
+
+    };
+
+    const handleChangeChainB = (event: ChangeEvent<HTMLSelectElement>) => {
+        console.log("chain dropdown2: ", event.target.value)
+        let chainToUse = Number(event.target.value);
+        setChainTo(chainToUse)
+        setChainFrom(chainToUse == 0 ? 1 : 0)
+    };
 
     return (
         <div className="swap-container">
@@ -86,6 +124,50 @@ export const ContentBox = () => {
                         <span>1 {fromToken.symbol} = 1,800.54 {toToken.symbol}</span>
                     </div>
                 </div> */}
+
+                <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={2}
+                    sx={{ width: '100%' }}
+                >
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl>
+                            <FormLabel>From</FormLabel>
+                            <Select
+                                value={chainFrom}
+                                onChange={(_, value) => {
+                                    if (value !== null) {
+                                        setChainTo(value === '0' ? '1' : '0');
+                                        setChainFrom(value);
+                                    }
+                                }}
+                            >
+                                <Option value="0">Devnet 0</Option>
+                                <Option value="1">Devnet 1</Option>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box sx={{ minWidth: 120 }}>
+                        <FormControl>
+                            <FormLabel>To</FormLabel>
+                            <Select
+                                value={chainTo}
+                                onChange={(_, value) => {
+                                    if (value !== null) {
+                                        setChainFrom(value === '0' ? '1' : '0');
+                                        setChainTo(value);
+                                    }
+                                }}
+                                disabled
+                            >
+                                <Option value="0">Devnet 0</Option>
+                                <Option value="1">Devnet 1</Option>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Stack>
 
                 <button className="swap-action-button">
                     Execute Strategy
